0be61640e254846fb301fbc4d2dac309544786b9
mbowey <michael.bowey@gmail.com>
2021-10-30 13:29:34 -0400

    all tests passed

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -52,10 +52,11 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -72,7 +73,7 @@ contract SimpleBank {
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
@@ -83,6 +84,13 @@ contract SimpleBank {
       // 4. Emit the appropriate event associated with this function
 
       // 5. return the balance of sndr of this transaction
+      if (enrolled[msg.sender]== true) {
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
+      }
+
+
     }
 
     /// @notice Withdraw ether from bank
@@ -101,5 +109,10 @@ contract SimpleBank {
       //    sender's balance
 
       // 3. Emit the appropriate event for this message
+      require (enrolled[msg.sender] == true && balances[msg.sender] >= withdrawAmount);
+      msg.sender.transfer(withdrawAmount);
+      balances[msg.sender] -= withdrawAmount;
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+      return balances[msg.sender];
     }
 }
